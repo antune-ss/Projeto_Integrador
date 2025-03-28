ce57e6b66519c808aca5cb68300ba97c783aba97
Gustavo Antunes <guantunes567@gmail.com>
2025-03-28 20:25:04 -0300

    Delete Menu.py

--- Menu.py
+++
@@ -1,8 +0,0 @@
-# SISTEMA DE SUSTENTABILIDADE PESSOAL - PROJETO INTEGRADOR 1
-# PROF: MARCELO TRAINA CHACON
-
-# Diogo Gomes dos Santos Ferreira - 23011569
-# Gustavo Antunes - 25013281
-# Lucas Henrique Ferreira - 25005444
-# Pedro Henrique de Oliveira - 25006386
-# Yuri Kauan Moreira de Almeida - 25004132
