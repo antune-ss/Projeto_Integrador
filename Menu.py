1742995748b6bab4e93812b71218167a88cd9daf
Gustavo Antunes <guantunes567@gmail.com>
2025-03-28 21:58:24 -0300

    Adicionando input de data

--- Menu.py
+++ Menu.py
@@ -7,4 +7,16 @@
 # Pedro Henrique de Oliveira - 25006386
 # Yuri Kauan Moreira de Almeida - 25004132
 
+print("SISTEMA DE SUSTENTABILIDADE PESSOAL")
 
+from datetime import datetime
+
+# Solicitar a data ao usuário no formato 'dd/mm/aaaa'
+data_str = input("Digite a data atual (dd/mm/aaaa): ")
+
+# Converter a string em um objeto datetime
+try:
+    data = datetime.strptime(data_str, "%d/%m/%Y")
+    print(f"Data informada: {data.strftime('%d/%m/%Y')}")
+except ValueError:
+    print("Formato de data inválido. Por favor, digite a data no formato correto: dd/mm/aaaa")
